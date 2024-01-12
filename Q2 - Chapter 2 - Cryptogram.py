7e51f7e8d313316378d93bdaeedee88bb63807df
Bricke <153592314+BrickeAC@users.noreply.github.com>
2024-01-12 19:20:24 +1100

    first commit

--- Q2 - Chapter 2 - Cryptogram.py
+++ Q2 - Chapter 2 - Cryptogram.py
@@ -1,4 +1,16 @@
+def decrpyt_funct():
+
 text = """VZ FRYSVFU VZCNGVRAG NAQ N YVGGYR VAFRPHER V ZNXR ZVFGNXRF V NZ BHG BS PBAGEBY 
 NAQNG GVZRF UNEQ BG UNAQYR OHG VS LBH PNAG UNAQYR ZR NG ZL JBEFG GURA LBH FHER NF 
 URYYQBAG QRFREIR ZR NG ZL ORFG ZNEVYLA ZBAEBR"""
 
+result = ""
+
+key = 13
+
+for char in text:
+    result += chr((ord(char) = key - 64) % 26 = 65)
+else:
+    result += chr((ord(char) + key - 96) % 26 + 97)
+
+print(encrypt_funct(txt, s))
